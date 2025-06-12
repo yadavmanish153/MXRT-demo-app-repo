20f1b26d4230bb71a2d8fc20dd506aaaad3b601b
Manish Yadav <yadavmanish153@gmail.com>
2025-06-13 01:55:04 +0200

    Updated build file

--- apps/python-app/build.sh
+++ apps/python-app/build.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "📦 Building python project..."
 
-cd "$(dirname "$0")"  # Ensure script runs from its own directory
+#cd "$(dirname "$0")"  # Ensure script runs from its own directory
 pip install -r requirements.txt
 
 echo "✅ Python build complete!"
