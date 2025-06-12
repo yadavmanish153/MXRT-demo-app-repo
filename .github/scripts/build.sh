d1640deb0e64b01f5c2112794f2d08cd795a2bf6
Manish Yadav <yadavmanish153@gmail.com>
2025-06-13 00:58:54 +0200

    Added ls command

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -8,6 +8,7 @@ find "$ROOT_DIR" -type f -name "build.sh" | while read -r build_script; do
     # Get the directory of the build.sh script
     dir=$(dirname "$build_script")
     echo "Executing build.sh in $dir"
+    ls
     
     # Change to that directory
     (cd "$dir" && chmod +x build.sh && ./build.sh)
