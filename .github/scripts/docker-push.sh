fdb33c7b9838e58a3e54c12b1718845ed6992d32
Manish Yadav <yadavmanish153@gmail.com>
2025-06-13 13:51:38 +0200

    Added Dockerfile and build image

--- .github/scripts/docker-push.sh
+++ .github/scripts/docker-push.sh
@@ -20,8 +20,7 @@ echo "$PAT" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker images
 
 # Get all image names that belong to your Docker Hub username
-images=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -v "<none>")
-
+images=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep "^$DOCKER_USERNAME/")
 
 # Push each image
 for image in $images; do
