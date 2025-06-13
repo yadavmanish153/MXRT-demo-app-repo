cd98ad4b676ee61bb913c7209b11a3d85ee6c62f
Manish Yadav <yadavmanish153@gmail.com>
2025-06-13 14:21:00 +0200

    Added Dockerfile and build image

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -28,7 +28,7 @@ find "$ROOT_DIR" -type f -name "Dockerfile" | while read -r build_script; do
     echo "image_name is $image_name"
     docker build -t $image_name:$tag .
     echo "image generated $image_name:$tag"
-    docker tag $image_name:$tag $username/$image_name:$TAG
+    #docker tag $image_name:$tag $DOCKER_USERNAME/$image_name:$TAG
     
     # Optional: check if it succeeded
     if [ $? -ne 0 ]; then
