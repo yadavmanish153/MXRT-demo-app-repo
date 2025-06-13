3e146ede52c200d5ba08706333b9f8db6e49cf05
Manish Yadav <yadavmanish153@gmail.com>
2025-06-13 13:48:00 +0200

    Added Dockerfile and build image

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -2,12 +2,13 @@
 
 # Check if at least 3 arguments are passed
 if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 <docker-username>"
+    echo "Usage: $0 <docker-username> <tag>"
     exit 1
 fi
 
 # Set your Docker Hub username
 DOCKER_USERNAME="$1"
+TAG="$2"
 
 # Root directory to start searching from (can also be set to a specific path)
 ROOT_DIR="./apps"
@@ -25,7 +26,7 @@ find "$ROOT_DIR" -type f -name "Dockerfile" | while read -r build_script; do
     tag="latest"
     echo "image_name is $image_name"
     docker build -t $image_name:$tag .
-    docker tag $image_name:$tag "$DOCKER_USERNAME"/$image_name:$tag
+    docker tag $image_name:$TAG $DOCKER_USERNAME/$image_name:$tag
     
     # Optional: check if it succeeded
     if [ $? -ne 0 ]; then
